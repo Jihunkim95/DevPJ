16752f99e194a93f41139b6930a9b6220d99680b
Jihunkim95 <darak551@naver.com>
2023-11-03 16:48:34 +0900

    첫커밋

---
+++ DevPJTest/DevPJTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DevPJTest
+//
+//  Created by 김지훈 on 2023/11/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DevPJTest/DevPJTest/DevPJTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevPJTestApp.swift
+//  DevPJTest
+//
+//  Created by 김지훈 on 2023/11/03.
+//
+
+import SwiftUI
+
+@main
+struct DevPJTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
